362c5377dd333663b470e7df9d0d61dac9238bd9
Rev1le <r.nikiy@yandex.ru>
2023-03-11 22:25:33 +0500

    Update frontend

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,17 @@ function App() {
     setOpenFileState( await invoke("open_file", {chunk_size: 256}) )
   };
 
-
-
   return (
 
     <div class={styles.App}>
-
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
 
         <button onClick={openFile}>Click Me</button>
-
         <Show when={openFileState() == true} fallback={<div>Загрузка...</div>}>
-        <div class = {styles.bytes_table}>
-          <p>Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\nТаблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\nТаблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\nТаблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\nТаблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\nТаблица1Таблица1Таблица1Таблица1Таблица1Таблица1Таблица1\n</p>
-          <p>Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\nТаблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\nТаблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\nТаблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\nТаблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\nТаблица2Таблица2Таблица2Таблица2Таблица2Таблица2Таблица2\n</p>
-          <p>Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\nТаблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\nТаблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\nТаблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\nТаблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\nТаблица3Таблица3Таблица3Таблица3Таблица3Таблица3Таблица3\n</p>
-          </div>
+          <CreateTable />
         </Show>
 
       </header>
@@ -44,84 +35,197 @@ function App() {
 
 
 function CreateTable() {
+  const [PrevChunk, setPrevChunk] = createSignal(new Array(256));
+  const [CurrentChunk, setCurrentChunk] = createSignal(new Array(256));
+  const [NextChunk, setNextChunk] = createSignal(new Array(256));
 
+  new Promise(
+    function(resolve, reject) {
+      const chunk = invoke("next_chunk", {});
+      resolve(chunk);
+    }
+  ).then(function(result) {
+    console.log(result);
+    setPrevChunk(result)
+  });
 
+  new Promise(
+    function(resolve, reject) {
+      const chunk = invoke("next_chunk", {});
+      resolve(chunk);
+    }
+  ).then(function(result) {
+    console.log(result);
+    setCurrentChunk(result);
+  });
 
-  const [currentChunk, setCurrentChunk] = createSignal(new Array(256));
-  const [nextChunk, setNextChunk] = createSignal(new Array(256));
-  const [prevChunk, setPrevChunk] = createSignal(new Array(256));
+  new Promise(
+    function(resolve, reject) {
+      const chunk = invoke("next_chunk", {});
+      resolve(chunk);
+    }
+  ).then(function(result) {
+    console.log(result);
+    setNextChunk(result)
+  });
 
-  let lastKnownScrollPosition = 0;
-  let ticking = false;
+  //console.log(PrevChunk(), CurrentChunk(), NextChunk());
+  const table = (
+    <div style="height:250px; overflow: auto;">
+      <div class = {styles.bytes_table} id = "bytes_table" style="height:2000px">
+        <div id = "free_space" style="height: 0px" />
+        <div style="height: 200px">
+          <PrevChunk />
+        </div>
+        <div style="height: 200px">
+          <CurrentChunk/>
+        </div>
+        <div style="height: 200px">
+        
+        </div>
+      </div>
+    </div>
+  );
 
-  document.addEventListener("scroll", (event) => {
-  lastKnownScrollPosition = window.scrollY;
+  const [PrevChunkScrollPos, setPrevChunkScrollPos] = createSignal(0);
+  const [heightFreeSpace, setHeightFreeSpace] = createSignal(0);
 
-  const next_chunk = async () => setChunk( await invoke("next_chunk_split", {}) );
+  //const free_space = table.getElementById("free_space");
 
-  if (!ticking) {
-    window.requestAnimationFrame(() => {
-      console.log(lastKnownScrollPosition);
+  //table.scrollTo(0, 200);
 
-      if (lastKnownScrollPosition > 380) {
-        lazy(next_chunk())
-        window.scrollTo(0, 350)
-      };
+  table.addEventListener("scroll", (event) => {
 
-      ticking = false;
-    });
+    if (table.scrollTop > PrevChunkScrollPos() + 200) {
+      for (const child of table.firstElementChild.children) {
+        if (child.id == "free_space") {
+          setHeightFreeSpace(heightFreeSpace() + 200);
+          child.style.height = heightFreeSpace() + "px";
+          table.scrollTo(0, table.scrollTop-200);
 
-    ticking = true;
-  }
-});
+          setPrevChunk(CurrentChunk());
+          setCurrentChunk(NextChunk());
 
-  // setInterval(async () => {
-  //   let new_arr = await invoke("next_chunk", {});
-  //   setChunk(new_arr);
-  //   console.log(chunk());
-  //   //window.resizeTo(20000, 20000);
-  // }, 5000);
+          new Promise(
+            function(resolve, reject) {
+              invoke("next_chunk", {})
+            }
+          ).then(setNextChunk);
 
-  let t =  <div class={styles.bytes_table} id = "bytes_table">
-  <button onClick = {async () => setCurrentChunk( await invoke("next_chunk_split", {}))} >NextChunk</button>
-  <table>
-    <tbody>
-
-    <For each={currentChunk()} fallback={<div>Загрузка...</div>}>
-      {(item_i, index_i) => (
-        <tr>
-        <td>Line{index_i()}</td>
-
-        <For each={item_i}>
-        {(item_j, index_j) => {
-          let byte = parseInt(item_j).toString(16);
-
-          if (byte.length == 1) {
-            byte = "0" + byte;
-          }
-
-          return <td>{byte}</td>
         }
-
       }
-        </For>
-        </tr>
-      )}
-    </For>
 
-    <tr>
-    <td>None</td>
-    </tr>
-    <tr>
-    <td>None</td>
-    </tr>
-    <tr>
-    <td>None</td>
-    </tr>
+      console.log("next chunk");
+      setPrevChunkScrollPos(table.scrollTop);
+    }
 
-    </tbody>
-  </table>
-  </div>
+    // if (table.scrollTop > PrevChunkScrollPos() + 200) {
+    //   setPrevChunkScrollPos(table.scrollTop)
+    //
+    //   for (const child of table.firstElementChild.children) {
+    //       if (child.id == "free_space") {
+    //           setHeightFreeSpace(heightFreeSpace() + 200);
+    //           child.style.height = heightFreeSpace() + "px";
+    //       }
+    //   }
+    //
+    //   console.log("next_chunk");
+    // }
+    // else if (table.scrollTop < PrevChunkScrollPos() - 200) {
+    //   setPrevChunkScrollPos(table.scrollTop);
+    //
+    //   for (const child of table.firstElementChild.children) {
+    //       if (child.id == "free_space") {
+    //         setHeightFreeSpace(heightFreeSpace() - 200);
+    //         child.style.height = heightFreeSpace() + "px";
+    //       }
+    //   }
+    //
+    //   console.log("prev_chunk");
+    // }
+
+  });
+
+  return table;
 }
+
+// function OldCreateTable() {
+//
+//   const [currentChunk, setCurrentChunk] = createSignal(new Array(256));
+//   const [nextChunk, setNextChunk] = createSignal(new Array(256));
+//   const [prevChunk, setPrevChunk] = createSignal(new Array(256));
+//
+//   let lastKnownScrollPosition = 0;
+//   let ticking = false;
+//
+//   document.addEventListener("scroll", (event) => {
+//   lastKnownScrollPosition = window.scrollY;
+//
+//   const next_chunk = async () => setChunk( await invoke("next_chunk_split", {}) );
+//
+//   if (!ticking) {
+//     window.requestAnimationFrame(() => {
+//       console.log(lastKnownScrollPosition);
+//
+//       if (lastKnownScrollPosition > 380) {
+//         lazy(next_chunk())
+//         window.scrollTo(0, 350)
+//       };
+//
+//       ticking = false;
+//     });
+//
+//     ticking = true;
+//   }
+// });
+//
+//   // setInterval(async () => {
+//   //   let new_arr = await invoke("next_chunk", {});
+//   //   setChunk(new_arr);
+//   //   console.log(chunk());
+//   //   //window.resizeTo(20000, 20000);
+//   // }, 5000);
+//
+//   let t =  <div class={styles.bytes_table} id = "bytes_table">
+//   <button onClick = {async () => setCurrentChunk( await invoke("next_chunk_split", {}))} >NextChunk</button>
+//   <table>
+//     <tbody>
+//
+//     <For each={currentChunk()} fallback={<div>Загрузка...</div>}>
+//       {(item_i, index_i) => (
+//         <tr>
+//         <td>Line{index_i()}</td>
+//
+//         <For each={item_i}>
+//         {(item_j, index_j) => {
+//           let byte = parseInt(item_j).toString(16);
+//
+//           if (byte.length == 1) {
+//             byte = "0" + byte;
+//           }
+//
+//           return <td>{byte}</td>
+//         }
+//
+//       }
+//         </For>
+//         </tr>
+//       )}
+//     </For>
+//
+//     <tr>
+//     <td>None</td>
+//     </tr>
+//     <tr>
+//     <td>None</td>
+//     </tr>
+//     <tr>
+//     <td>None</td>
+//     </tr>
+//
+//     </tbody>
+//   </table>
+//   </div>
+// }
 
 export default App;
